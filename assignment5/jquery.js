b09ae69904fd72404c968feb0414596e2775dd48
SolomonV01 <113135388+SolomonV01@users.noreply.github.com>
2023-03-03 12:08:19 -0600

    assignment5

---
+++ assignment5/jquery.js
@@ -0,0 +1,34 @@
+//alert("Welcome to GHOST!")
+
+
+
+function myFunction() {
+  var x = document.getElementById("hidden");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+function myImg() {
+  var x = document.getElementById("hiddenimg");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+function song() {
+  var x = document.getElementById("audio");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
