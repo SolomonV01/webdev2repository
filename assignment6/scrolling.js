1d8cd9aa1e33fa9dd85d55d3ef461ff328c26c3c
SolomonV01 <113135388+SolomonV01@users.noreply.github.com>
2023-03-23 21:40:39 -0500

    scriptanimation

--- assignment6/scrolling.js
+++ assignment6/scrolling.js
@@ -1,20 +1,14 @@
-$(window).scroll(function() {
-    var $window = $(window),
-        $body = $('body'),
-    
-    var scroll = $window.scrollTop() + ($window.height() / 3);
-   
-    $panel.each(function () {
-      var $this = $(this);
-      
-      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            
-        $body.removeClass(function (index, css) {
-          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-        });
-         
-        $body.addClass('section' + $(this).data('section'));
-      }
-    });    
-    
-  }).scroll();
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+    else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
