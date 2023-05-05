b81f2b0bd349178ecf08a923a33a07afabf00f03
SolomonV01 <113135388+SolomonV01@users.noreply.github.com>
2023-05-05 14:26:52 -0500

    hiddentext

---
+++ final-project/js.js
@@ -0,0 +1,30 @@
+function erning() {
+    var x = document.getElementById("hiddenE");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+  
+
+
+function cecilia() {
+    var x = document.getElementById("hiddenC");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+  
+
+
+function zelda() {
+    var x = document.getElementById("hiddenZ");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
